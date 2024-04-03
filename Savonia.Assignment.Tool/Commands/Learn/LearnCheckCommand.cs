8212e71486b27880538ec1a26f8c329d65b9c3ad
Mikko Pääkkönen <mikko.paakkonen@savonia.fi>
2024-04-03 18:01:39 +0300

    working on bulk check achievements

--- Savonia.Assignment.Tool/Commands/Learn/LearnCheckCommand.cs
+++ Savonia.Assignment.Tool/Commands/Learn/LearnCheckCommand.cs
@@ -13,13 +13,31 @@ namespace Savonia.Assignment.Tool.Commands.Learn;
 
 public class LearnCheckCommand : Command
 {
-    
+
     public LearnCheckCommand() : base("check", "Check that required achievements are completed for users.")
     {
         var requirementsArgument = new Argument<FileInfo>(
             name: "requirementsJsonFile",
-            description: "JSON file containing required achievements as JSON key-value-pairs where the key is achiements type id and value is achievement title."
-            );
+            description: "JSON file containing required achievements as JSON key-value-pairs where the key is achiements type id and value is achievement title.",
+            parse: result =>
+            {
+                if (result.Tokens.Count == 0)
+                {
+                    result.ErrorMessage = "Requirements JSON file is not specified.";
+                    return null;
+                }
+                string? filePath = result.Tokens.Single().Value;
+                if (!File.Exists(filePath))
+                {
+                    result.ErrorMessage = "Requirements JSON file does not exist";
+                    return null;
+                }
+                else
+                {
+                    return new FileInfo(filePath);
+                }
+            });
+        
         var outputOption = new Option<string?>(
             name: "--output",
             description: "Output csv file. This will overwrite possible existing file.",
@@ -36,27 +54,47 @@ public class LearnCheckCommand : Command
             description: "Delimiter for the input CSV file.",
             getDefaultValue: () => ",");
 
-        var inputCsvDelimiterOption = new Option<string>(
-            name: "--input-delimiter",
-            description: "Delimiter for the input CSV file.",
-            getDefaultValue: () => ",");
+        var usernameFieldOption = new Option<string>(
+            name: "--username-field",
+            description: "Field where the username is read. Use one-based (i.e. first field is one (1)) field number or field header to select. Defaults to field number 9 which is default field for Moodle LMS export file for assingment submissions text.",
+            getDefaultValue: () => "9");
+
+        var gradeFieldOption = new Option<string>(
+            name: "--grade-field",
+            description: "Field where the grade is written. Use one-based (i.e. first field is one (1)) field number or field header to select. Defaults to field number 5 which is default field for Moodle LMS export file for assingment grade.",
+            getDefaultValue: () => "5");
+
+        var commentsFieldOption = new Option<string>(
+            name: "--comments-field",
+            description: "Field where the comments is written. Use one-based (i.e. first field is one (1)) field number or field header to select. Defaults to field number 11 which is default field for Moodle LMS export file for assingment feedback comments.",
+            getDefaultValue: () => "11");
 
         Add(CommonArguments.SourceCsvFileArgument);
+        Add(requirementsArgument);
         AddOption(outputOption);
+        AddOption(inputCsvHasHeaderOption);
+        AddOption(inputCsvDelimiterOption);
+        AddOption(usernameFieldOption);
+        AddOption(gradeFieldOption);
+        AddOption(commentsFieldOption);
 
         this.SetHandler(async (context) =>
             {
-                string input = context.ParseResult.GetValueForArgument(usernameArgument)!;
-                string? output = context.ParseResult.GetValueForOption(outputOption);
+                FileInfo input = context.ParseResult.GetValueForArgument(CommonArguments.SourceCsvFileArgument)!;
+                string? output = context.ParseResult.GetValueForOption(outputOption) ?? $"{Path.GetFileNameWithoutExtension(input.Name)}-result.csv";
 
                 await Handle(input, output,
-                                 context.ParseResult.GetValueForOption(GlobalOptions.VerboseOption));
+                                context.ParseResult.GetValueForArgument(requirementsArgument)!,
+                                context.ParseResult.GetValueForOption(inputCsvDelimiterOption)!,
+                                context.ParseResult.GetValueForOption(inputCsvHasHeaderOption),
+                                context.ParseResult.GetValueForOption(usernameFieldOption),
+                                context.ParseResult.GetValueForOption(gradeFieldOption),
+                                context.ParseResult.GetValueForOption(commentsFieldOption),
+                                context.ParseResult.GetValueForOption(GlobalOptions.VerboseOption));
             });
     }
 
-    async Task Handle(string username,
-                        string? output,
-                        bool verbose)
+    async Task Handle(FileInfo input, string? output, FileInfo requirementsJsonFile, string inputDelimiter, bool inputHasHeader, string usernameField, string gradeField, string commentsField, bool verbose)
     {
         // set working directory
         Console.WriteLine($"Reading achievements for user {username}");
@@ -95,6 +133,19 @@ public class LearnCheckCommand : Command
             Console.WriteLine($"Modules:\n{string.Join("\n", modules.OrderBy(a => a.Title).Select(a => $"- {a}"))}");
             Console.WriteLine();
             Console.WriteLine($"Total count: {achievements.TotalCount}");
+        }
+    }
+
+    private int GetFieldIndex(List<string> headerRow, string fieldName)
+    {
+        if (int.TryParse(fieldName, out int fieldNumber) && fieldNumber > 0 && fieldNumber <= headerRow.Count)
+        {
+            return fieldNumber - 1;
+        }
+        else
+        {
+            var fieldIndex = headerRow.IndexOf(fieldName);
+            return fieldIndex;
         }
     }
 }
